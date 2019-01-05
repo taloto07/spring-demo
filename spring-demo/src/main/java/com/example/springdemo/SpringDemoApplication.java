679a0a96719c2ea8bd0220b865e1c91e3f4ffdd3
chamnaplim <chamnaplim@192.168.0.10>
2019-01-05 08:48:03 -0800

    change SpringDemoApplication.java to original stage

--- spring-demo/src/main/java/com/example/springdemo/SpringDemoApplication.java
+++ spring-demo/src/main/java/com/example/springdemo/SpringDemoApplication.java
@@ -1,15 +1,8 @@
 package com.example.springdemo;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-
-import com.example.springdemo.library.cards.ICard;
-import com.example.springdemo.library.cards.SingleValueCard;
-import com.example.springdemo.library.enums.SuitType;
 
 @SpringBootApplication
 public class SpringDemoApplication {
@@ -18,12 +11,6 @@ public class SpringDemoApplication {
 		ConfigurableApplicationContext context = SpringApplication.run(SpringDemoApplication.class, args);
 		
 		context.getBean(GameManager.class).start();
-		
-		
-		ArrayList<ICard> arrayList = new ArrayList<>();
-		arrayList.add(new SingleValueCard(1, SuitType.Club));
-		arrayList.add(new SingleValueCard(2, SuitType.Diamond));
-		List<ICard> list = arrayList;
 		
 	}
 
